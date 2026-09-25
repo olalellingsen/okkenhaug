3fa96d26bebab0fc110fee52cad9a728977b3a13
Ola Lømo Ellingsen <ola.l.ellingsen@gmail.com>
2026-09-25 13:10:35 +0200

    baseline for okkenhaug film

--- src/app/(site)/components/PortableTextSection.tsx
+++ src/app/(site)/components/PortableTextSection.tsx
@@ -1,7 +1,6 @@
 import { PortableText, PortableTextComponents } from "@portabletext/react";
 import { RichTextBlock } from "../../types";
 import Button from "./Button";
-import { ExternalLink } from "lucide-react";
 import SanityImage from "./SanityImage";
 
 // Type for image values in portable text
@@ -69,7 +68,6 @@ const stylings: PortableTextComponents = {
         className="text-foreground inline-flex"
       >
         {children}
-        <ExternalLink className="ml-1 mt-1 size-4" />
       </Button>
     ),
   },
